81ff6d526d2fcbfd7a525cb9b5d18e000545bab7
Chris Howson <chris.howson@trueclarity.co.uk>
2015-10-27 10:14:41 +0000

    Moved device resolving to rules based field

---
+++ Sitecore.51Degrees.CloudDeviceDetection/Sitecore/Pipelines/HttpRequestBegin/DeviceDetection/DeviceConditionProcessor.cs
@@ -0,0 +1,47 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.Rules;
+
+namespace Sitecore.FiftyOneDegrees.CloudDeviceDetection.Sitecore.Pipelines.HttpRequestBegin.DeviceDetection
+{
+    public class DeviceConditionProcessor : ResolveMobileDeviceProcessor
+    {
+        public override void Process(ResolveMobileDevicePipelineArgs args)
+        {
+            var matchingDevice = ExecuteDeviceConditions();
+
+            if (matchingDevice != null)
+            {
+                args.DeviceId = matchingDevice.ID.ToString();
+            }
+        }
+
+        private static DeviceItem ExecuteDeviceConditions()
+        {
+            var devices = Context.Database.Resources.Devices.GetAll();
+
+            foreach (var device in devices)
+            {
+                var ruleContext = new RuleContext();
+
+                foreach (var rule in RuleFactory.GetRules<RuleContext>(new[] { device.InnerItem }, "Condition").Rules)
+                {
+                    if (rule.Condition != null)
+                    {
+                        var stack = new RuleStack();
+                        rule.Condition.Evaluate(ruleContext, stack);
+                        if (ruleContext.IsAborted)
+                        {
+                            continue;
+                        }
+                        if ((stack.Count != 0) && ((bool)stack.Pop()))
+                        {
+                            return device;
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
--- Sitecore.51Degrees.CloudDeviceDetection/Sitecore/Pipelines/HttpRequestBegin/DeviceDetection/DeviceIdResolvingProcessor.cs
+++ Sitecore.51Degrees.CloudDeviceDetection/Sitecore/Pipelines/HttpRequestBegin/DeviceDetection/DeviceIdResolvingProcessor.cs
@@ -1,10 +1,12 @@
-﻿using Sitecore.FiftyOneDegrees.CloudDeviceDetection.Data;
+﻿using System;
+using Sitecore.FiftyOneDegrees.CloudDeviceDetection.Data;
 using Sitecore.FiftyOneDegrees.CloudDeviceDetection.Factories;
 using Sitecore.FiftyOneDegrees.CloudDeviceDetection.Services;
 using Sitecore.FiftyOneDegrees.CloudDeviceDetection.Settings;
 
 namespace Sitecore.FiftyOneDegrees.CloudDeviceDetection.Sitecore.Pipelines.HttpRequestBegin.DeviceDetection
 {
+    [Obsolete]
     public class DeviceIdResolvingProcessor : ResolveMobileDeviceProcessor
     {
         public override void Process(ResolveMobileDevicePipelineArgs args)
